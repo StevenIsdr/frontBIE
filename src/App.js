b19a6e744d6eaab4d16946070e6b9566c76a94ff
StevenIsdr <laglobule59260@gmail.com>
2022-02-01 17:08:48 +0100

    FIni presenciel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./Navbar";
+import Navbar from "./Navbar.js"
 
 function App() {
-  return (
-      <Navbar></Navbar>
-  );
+    return (
+        <div>
+            <Navbar></Navbar>
+        <main>
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                {/* <!-- Replace with your content --> */}
+                <div className="px-4 py-6 sm:px-0">
+                    Content
+                </div>
+                {/* <!-- /End replace --> */}
+            </div>
+        </main>
+    </div>);
 }
 
 export default App;
---
+++ src/Register.js
@@ -0,0 +1,105 @@
+import './App.css';
+import Navbar from "./Navbar";
+import {useState} from "react";
+
+function Register(){
+    const [nom, setNom] = useState("");
+    const [prenom, setPrenom] = useState("");
+    const [email, setEmail] = useState("");
+    const [motdepasse, setMdp] = useState("");
+    const [offre, setOffre] = useState("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch("https://httpbin.org/post", {
+                method: "POST",
+                body: JSON.stringify({
+                    nom: nom,
+                    prenom: prenom,
+                    email: email,
+                    motdepasse: motdepasse,
+                    offre: offre
+                }),
+            });
+            let resJson = await res.json();
+            if (res.status === 200) {
+                console.log("User created successfully");
+            } else {
+                console.log("Some error occured");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+
+
+    return (
+        <div>
+            <Navbar></Navbar>
+
+            <div className="h-screen flex">
+                <div className="flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center">
+                    <div>
+                        <h1 className="text-white font-bold text-4xl font-sans">XENODriver</h1>
+                        <p className="text-white mt-1">Heberger facilement vos photos et vidéos !</p>
+                        <button type="submit" className="block w-40 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">En savoir plus !</button>
+                    </div>
+                </div>
+                <div className="flex w-1/2 justify-center items-center bg-white">
+                    <form className="bg-white" onSubmit={handleSubmit}>
+                        <h1 className="text-gray-800 font-bold text-2xl mb-1">Bonjour,</h1>
+                        <p className="text-sm font-normal text-gray-600 mb-7">Inscription</p>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                                 fill="currentColor">
+                                <path fill-rule="evenodd"
+                                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="text" value={prenom} onChange={(e) => setPrenom(e.target.value)} placeholder="Prenom" />
+                        </div>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                                 fill="currentColor">
+                                <path fill-rule="evenodd"
+                                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="text" value={nom} onChange={(e) => setNom(e.target.value)} placeholder="Nom" />
+                        </div>
+
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Adresse Mail" />
+                        </div>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                            </svg>
+                            <input className="pl-2 outline-none border-none" type="password" value={motdepasse} onChange={(e) => setMdp(e.target.value)} placeholder="Mot de passe" />
+                        </div>
+                        <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+                                 fill="currentColor">
+                                <path fill-rule="evenodd"
+                                      d="M3 4a1 1 0 011-1h4a1 1 0 010 2H6.414l2.293 2.293a1 1 0 01-1.414 1.414L5 6.414V8a1 1 0 01-2 0V4zm9 1a1 1 0 110-2h4a1 1 0 011 1v4a1 1 0 11-2 0V6.414l-2.293 2.293a1 1 0 11-1.414-1.414L13.586 5H12zm-9 7a1 1 0 112 0v1.586l2.293-2.293a1 1 0 011.414 1.414L6.414 15H8a1 1 0 110 2H4a1 1 0 01-1-1v-4zm13-1a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 110-2h1.586l-2.293-2.293a1 1 0 011.414-1.414L15 13.586V12a1 1 0 011-1z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                            <select name="offer" id="offer-select" value={offre} onChange={(e) => setOffre(e.target.value)}>
+                                <option key="1" value="1">5Gb - Gratuit</option>
+                                <option key="2" value="2">100Gb - 5€</option>
+                                <option key="3" value="3">1Tb - 25€</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Inscription</button>
+                    </form>
+                </div>
+            </div>
+        </div>);
+}
+
+export default Register;
