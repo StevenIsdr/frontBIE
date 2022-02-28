b2d9051ea64f5f29af396b80557bbec5fdccfcb0
thomas cls <deanint59@gmail.com>
2022-02-28 10:45:22 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,35 @@
 import './App.css';
 import Navbar from "./Navbar";
+import {useState} from "react";
 
 function Login(){
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+
+    let handleSubmit = async (e) => {
+        console.log("connexion")
+        e.preventDefault();
+        try {
+            let res = await fetch("http://127.0.0.1:8000/api/login", {
+                method: "POST",
+                headers:{'content-type': 'application/json'},
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
+            console.log(res)
+            if (res.status === 200) {
+                console.log("User log successfully");
+            } else {
+                console.log("Some error occured");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <div>
             <Navbar></Navbar>
@@ -21,15 +49,15 @@ function Login(){
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                             </svg>
-                            <input className="pl-2 outline-none border-none" type="text" name id placeholder="Adresse Mail" />
+                            <input className="pl-2 outline-none border-none" type="text"  value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
                         </div>
                         <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                             </svg>
-                            <input className="pl-2 outline-none border-none" type="password" name id placeholder="Mot de passe" />
+                            <input className="pl-2 outline-none border-none" type="password"   value={password} onChange={(e) => setPassword(e.target.value)}  placeholder="Mot de passe" />
                         </div>
-                        <button type="submit" className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Connexion</button>
+                        <button onClick={handleSubmit} className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Connexion</button>
                     </form>
                 </div>
             </div>
